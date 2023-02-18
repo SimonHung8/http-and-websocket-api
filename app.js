db35ccba7476de615aa1119e3100f4d1d5a38e70
SimonHung8 <simon850226@gmail.com>
2023-02-18 10:11:23 +0800

    feat: add general error handler

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const routes = require('./routes')
+const { errorHandler, undefinedRoutes } = require('./middleware/error-handler')
 
 const app = express()
 const port = 3000
 
 app.use(routes)
+app.use(errorHandler)
+app.use('*', undefinedRoutes)
 
 app.listen(port, () => console.log('Listening on port 3000'))
