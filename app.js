65e1772e0ee9e5de0df73097014bdd13115515b4
SimonHung8 <simon850226@gmail.com>
2023-02-18 15:12:29 +0800

    feat: add websocket server

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const WebSocket = require('ws')
+const http = require('http')
 const routes = require('./routes')
 const { errorHandler, undefinedRoutes } = require('./middleware/error-handler')
 require('./config/redis')
 
 const app = express()
 const port = 3000
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server, path: '/streaming' })
 
 app.use(routes)
 app.use(errorHandler)
 app.use('*', undefinedRoutes)
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+wss.on('connection', ws => {
+  console.log('WebSocket connected')
+  ws.send('Welcome')
+  ws.on('close', () => console.log('connection closed'))
+})
+
+server.listen(port, () => console.log(`Listening on port ${port}`))
