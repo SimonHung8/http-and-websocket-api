638d9779c63fe23bcc6517ce3af607357a8edf64
SimonHung8 <simon850226@gmail.com>
2023-02-18 10:05:14 +0800

    feat: add data route

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => res.send('Hello World'))
+app.use(routes)
 
 app.listen(port, () => console.log('Listening on port 3000'))
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const fetch = require('node-fetch')
+
+router.get('/data', async (req, res, next) => {
+  try {
+    const TARGET_URL = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
+    const targetRes = await fetch(TARGET_URL)
+    if (targetRes.status !== 200) {
+      return res.status(500).json({ message: 'fail to fetch' })
+    }
+    const result = await targetRes.json()
+    return res.status(200).json({ result })
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
